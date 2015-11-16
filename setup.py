94ca1f005d2fa2cec2009615441515438cfa8dd9
vad <vadivelmurugank@gmail.com>
2015-11-16 22:44:24 +0000

    version 0.21

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
   name = 'calltree',
   packages_dir = {'calltree' : ' '},
   packages = ['calltree'],
-  version = '0.2',
-  description = 'Parse api and api groups from sources',
+  version = '0.21',
+  description = 'Show Callgraph from cscope database"
   author = 'Vadivel',
   author_email = 'vadivelmurugank@gmail.com',
   url = 'https://github.com/vadivelmurugank/calltree',
